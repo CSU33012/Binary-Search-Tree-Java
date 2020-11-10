8e37e1b09e032afd81a073fefb8776f88e238082
CSU33012 <never@caughtlack.in>
2020-11-10 20:46:38 +0000

    added skeleton of the junit test file

---
+++ DAG/src/JUnitTestDAG.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//-------------------------------------------------------------------------
+/**
+ *  
+ *
+ *  @author Darragh Murray
+ *  @version HT 2020
+ */
+@RunWith(JUnit4.class)
+public class JUnitTestDAG {
+    
+    @Test
+    public void testEmpty() {
+    	
+    }
+
+    @Test
+    public void testStandardBST() {
+
+    }
+    
+    @Test 
+    public void testSingleNode() {
+    	
+    }
+
+}
